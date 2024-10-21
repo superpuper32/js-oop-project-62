51b2188e3fce95f016d23b8191f6ce44a4eb7ef9
superpuper32 <pochitalin_m@mail.ru>
2024-10-22 00:02:04 +0300

    STEP-1: tests added

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -14,4 +14,8 @@ test('Validator', () => {
   expect(schema.isValid('hexlet')).toBe(true);
   expect(schema.isValid(null)).toBe(false);
   expect(schema.isValid('')).toBe(false);
+  expect(schema.contains('what').isValid('what does the fox say')).toBe(true);
+  expect(schema.contains('whatthe').isValid('what does the fox say')).toBe(false);
+  expect(schema.isValid('what does the fox say')).toBe(false);
+  expect(schema.minLength(10).minLength(4).isValid('Hexlet')).toBe(true);
 });
